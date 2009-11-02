dc47327d4950a945762df98d9102b19fabb1b9d1
Malthe Borch <mborch@gmail.com>
2009-11-03 00:34:46 +0100

    Test access denied.

--- otto/tests/test_app.py
+++ otto/tests/test_app.py
@@ -7,3 +7,16 @@ class ApplicationCase(unittest.TestCase):
         app = Application()
         self.assertEqual(get_response(app, '/path'), ['Page not found.'])
 
+    def test_forbidden(self):
+        from otto import Application
+        from otto.tests.utils import get_response
+        app = Application(lambda: {'path': None})
+        @app.route('/*')
+        def controller_with(request):
+            pass
+        import webob.exc
+        @app.on_traverse
+        def handler(context, name):
+            raise webob.exc.HTTPForbidden("Traversal disabled.")
+        self.assertEqual(get_response(app, '/path'), ['Access was denied.'])
+
