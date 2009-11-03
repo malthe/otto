2f61509e3143aa66996ab3d704ac43e0708901b9
Malthe Borch <mborch@gmail.com>
2009-11-03 14:54:24 +0100

    Be lenient.

--- otto/tests/mock/simple_server.py
+++ otto/tests/mock/simple_server.py
@@ -9,6 +9,6 @@ class make_server(object):
 
 def assert_response(url, app, value):
     response = get_response(app, url)
-    result = ''.join(response).strip()
-    value = value.strip()
+    result = ''.join(response).strip().replace(' ', '')
+    value = value.strip().replace(' ', '')
     assert result == value, "Response: %s does not match: %s" % (result, value)
